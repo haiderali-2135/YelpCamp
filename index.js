06e47c938372e47b878740c2a367850a31980b60
haiderali-2135 <101544110+haiderali-2135@users.noreply.github.com>
2023-10-02 01:41:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-const port = proces.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`serving on port ${port}`);
